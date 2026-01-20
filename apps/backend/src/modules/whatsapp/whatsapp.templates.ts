405036ad1d1f9ea2b5e4eb0ce7b70b2c36f0995a
aiyalsenthil-glitch <aiyalcomputers@gmail.com>
2026-01-20 15:07:44 +0530

    change whatsapp temp

--- apps/backend/src/modules/whatsapp/whatsapp.templates.ts
+++ apps/backend/src/modules/whatsapp/whatsapp.templates.ts
@@ -1,5 +1,5 @@
 export const WhatsAppTemplates = {
-  WELCOME: 'new_member_welcome_v1',
+  WELCOME: 'new_member_welcome_v3',
   EXPIRY: 'membership_expiry_reminder',
   PAYMENT_DUE: 'payment_due_notice_util_v1',
 };
